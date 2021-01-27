e2734ca5752d24be8744dfe6c5abc026da5be17b
shenmishajing <shenmishajing@Gmail.com>
2021-01-27 15:00:38 +0800

    reformat head.py and redesign Tensors.shape in head.py

--- projects/SparseRCNN/sparsercnn/head.py
+++ projects/SparseRCNN/sparsercnn/head.py
@@ -22,7 +22,6 @@ import torch.nn.functional as F
 from detectron2.modeling.poolers import ROIPooler, cat
 from detectron2.structures import Boxes
 
-
 _DEFAULT_SCALE_CLAMP = math.log(100000.0 / 16)
 
 
@@ -34,7 +33,7 @@ class DynamicHead(nn.Module):
         # Build RoI.
         box_pooler = self._init_box_pooler(cfg, roi_input_shape)
         self.box_pooler = box_pooler
-        
+
         # Build heads.
         num_classes = cfg.MODEL.SparseRCNN.NUM_CLASSES
         d_model = cfg.MODEL.SparseRCNN.HIDDEN_DIM
@@ -43,10 +42,10 @@ class DynamicHead(nn.Module):
         dropout = cfg.MODEL.SparseRCNN.DROPOUT
         activation = cfg.MODEL.SparseRCNN.ACTIVATION
         num_heads = cfg.MODEL.SparseRCNN.NUM_HEADS
-        rcnn_head = RCNNHead(cfg, d_model, num_classes, dim_feedforward, nhead, dropout, activation)        
+        rcnn_head = RCNNHead(cfg, d_model, num_classes, dim_feedforward, nhead, dropout, activation)
         self.head_series = _get_clones(rcnn_head, num_heads)
         self.return_intermediate = cfg.MODEL.SparseRCNN.DEEP_SUPERVISION
-        
+
         # Init parameters.
         self.use_focal = cfg.MODEL.SparseRCNN.USE_FOCAL
         self.num_classes = num_classes
@@ -82,10 +81,10 @@ class DynamicHead(nn.Module):
         assert len(set(in_channels)) == 1, in_channels
 
         box_pooler = ROIPooler(
-            output_size=pooler_resolution,
-            scales=pooler_scales,
-            sampling_ratio=sampling_ratio,
-            pooler_type=pooler_type,
+            output_size = pooler_resolution,
+            scales = pooler_scales,
+            sampling_ratio = sampling_ratio,
+            pooler_type = pooler_type,
         )
         return box_pooler
 
@@ -96,7 +95,7 @@ class DynamicHead(nn.Module):
 
         bboxes = init_bboxes
         proposal_features = init_features
-        
+
         for rcnn_head in self.head_series:
             class_logits, pred_bboxes, proposal_features = rcnn_head(features, bboxes, proposal_features, self.box_pooler)
 
@@ -113,14 +112,14 @@ class DynamicHead(nn.Module):
 
 class RCNNHead(nn.Module):
 
-    def __init__(self, cfg, d_model, num_classes, dim_feedforward=2048, nhead=8, dropout=0.1, activation="relu",
-                 scale_clamp: float = _DEFAULT_SCALE_CLAMP, bbox_weights=(2.0, 2.0, 1.0, 1.0)):
+    def __init__(self, cfg, d_model, num_classes, dim_feedforward = 2048, nhead = 8, dropout = 0.1, activation = "relu",
+                 scale_clamp: float = _DEFAULT_SCALE_CLAMP, bbox_weights = (2.0, 2.0, 1.0, 1.0)):
         super().__init__()
 
         self.d_model = d_model
 
         # dynamic.
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout = dropout)
         self.inst_interact = DynamicConv(cfg)
 
         self.linear1 = nn.Linear(d_model, dim_feedforward)
@@ -142,7 +141,7 @@ class RCNNHead(nn.Module):
         for _ in range(num_cls):
             cls_module.append(nn.Linear(d_model, d_model, False))
             cls_module.append(nn.LayerNorm(d_model))
-            cls_module.append(nn.ReLU(inplace=True))
+            cls_module.append(nn.ReLU(inplace = True))
         self.cls_module = nn.ModuleList(cls_module)
 
         # reg.
@@ -151,9 +150,9 @@ class RCNNHead(nn.Module):
         for _ in range(num_reg):
             reg_module.append(nn.Linear(d_model, d_model, False))
             reg_module.append(nn.LayerNorm(d_model))
-            reg_module.append(nn.ReLU(inplace=True))
+            reg_module.append(nn.ReLU(inplace = True))
         self.reg_module = nn.ModuleList(reg_module)
-        
+
         # pred.
         self.use_focal = cfg.MODEL.SparseRCNN.USE_FOCAL
         if self.use_focal:
@@ -164,7 +163,6 @@ class RCNNHead(nn.Module):
         self.scale_clamp = scale_clamp
         self.bbox_weights = bbox_weights
 
-
     def forward(self, features, bboxes, pro_features, pooler):
         """
         :param bboxes: (N, nr_boxes, 4)
@@ -172,22 +170,22 @@ class RCNNHead(nn.Module):
         """
 
         N, nr_boxes = bboxes.shape[:2]
-        
+
         # roi_feature.
         proposal_boxes = list()
         for b in range(N):
             proposal_boxes.append(Boxes(bboxes[b]))
-        roi_features = pooler(features, proposal_boxes)            
-        roi_features = roi_features.view(N * nr_boxes, self.d_model, -1).permute(2, 0, 1)        
+        roi_features = pooler(features, proposal_boxes)
+        roi_features = roi_features.view(N * nr_boxes, self.d_model, -1)
 
         # self_att.
-        pro_features = pro_features.view(N, nr_boxes, self.d_model).permute(1, 0, 2)
-        pro_features2 = self.self_attn(pro_features, pro_features, value=pro_features)[0]
+        pro_features = pro_features.permute(1, 0, 2)
+        pro_features2 = self.self_attn(pro_features, pro_features, value = pro_features)[0]
         pro_features = pro_features + self.dropout1(pro_features2)
         pro_features = self.norm1(pro_features)
 
         # inst_interact.
-        pro_features = pro_features.view(nr_boxes, N, self.d_model).permute(1, 0, 2).reshape(1, N * nr_boxes, self.d_model)
+        pro_features = pro_features.permute(1, 0, 2).reshape(N * nr_boxes, self.d_model)
         pro_features2 = self.inst_interact(pro_features, roi_features)
         pro_features = pro_features + self.dropout2(pro_features2)
         obj_features = self.norm2(pro_features)
@@ -196,10 +194,9 @@ class RCNNHead(nn.Module):
         obj_features2 = self.linear2(self.dropout(self.activation(self.linear1(obj_features))))
         obj_features = obj_features + self.dropout3(obj_features2)
         obj_features = self.norm3(obj_features)
-        
-        fc_feature = obj_features.transpose(0, 1).reshape(N * nr_boxes, -1)
-        cls_feature = fc_feature.clone()
-        reg_feature = fc_feature.clone()
+
+        cls_feature = obj_features.clone()
+        reg_feature = obj_features.clone()
         for cls_layer in self.cls_module:
             cls_feature = cls_layer(cls_feature)
         for reg_layer in self.reg_module:
@@ -207,9 +204,8 @@ class RCNNHead(nn.Module):
         class_logits = self.class_logits(cls_feature)
         bboxes_deltas = self.bboxes_delta(reg_feature)
         pred_bboxes = self.apply_deltas(bboxes_deltas, bboxes.view(-1, 4))
-        
-        return class_logits.view(N, nr_boxes, -1), pred_bboxes.view(N, nr_boxes, -1), obj_features
-    
+
+        return class_logits.view(N, nr_boxes, -1), pred_bboxes.view(N, nr_boxes, -1), obj_features.reshape(N, nr_boxes, -1)
 
     def apply_deltas(self, deltas, boxes):
         """
@@ -235,8 +231,8 @@ class RCNNHead(nn.Module):
         dh = deltas[:, 3::4] / wh
 
         # Prevent sending too large values into torch.exp()
-        dw = torch.clamp(dw, max=self.scale_clamp)
-        dh = torch.clamp(dh, max=self.scale_clamp)
+        dw = torch.clamp(dw, max = self.scale_clamp)
+        dh = torch.clamp(dh, max = self.scale_clamp)
 
         pred_ctr_x = dx * widths[:, None] + ctr_x[:, None]
         pred_ctr_y = dy * heights[:, None] + ctr_y[:, None]
@@ -266,7 +262,7 @@ class DynamicConv(nn.Module):
         self.norm1 = nn.LayerNorm(self.dim_dynamic)
         self.norm2 = nn.LayerNorm(self.hidden_dim)
 
-        self.activation = nn.ReLU(inplace=True)
+        self.activation = nn.ReLU(inplace = True)
 
         pooler_resolution = cfg.MODEL.ROI_BOX_HEAD.POOLER_RESOLUTION
         num_output = self.hidden_dim * pooler_resolution ** 2
@@ -275,14 +271,14 @@ class DynamicConv(nn.Module):
 
     def forward(self, pro_features, roi_features):
         '''
-        pro_features: (1,  N * nr_boxes, self.d_model)
-        roi_features: (49, N * nr_boxes, self.d_model)
+        pro_features: (N * nr_boxes, self.d_model)
+        roi_features: (N * nr_boxes, self.d_model, 49)
         '''
-        features = roi_features.permute(1, 0, 2)
-        parameters = self.dynamic_layer(pro_features).permute(1, 0, 2)
+        features = roi_features.permute(0, 2, 1)
+        parameters = self.dynamic_layer(pro_features)
 
-        param1 = parameters[:, :, :self.num_params].view(-1, self.hidden_dim, self.dim_dynamic)
-        param2 = parameters[:, :, self.num_params:].view(-1, self.dim_dynamic, self.hidden_dim)
+        param1 = parameters[:, :self.num_params].view(-1, self.hidden_dim, self.dim_dynamic)
+        param2 = parameters[:, self.num_params:].view(-1, self.dim_dynamic, self.hidden_dim)
 
         features = torch.bmm(features, param1)
         features = self.norm1(features)
